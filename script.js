667f739a2b4bcd3d6e8329373643f87b66cd5f44
Brent <jbmsharpe@gmail.com>
2023-01-05 17:00:39 -0500

    created home page and added navbar, hero, programs section, mission section

--- script.js
+++ script.js
@@ -2,6 +2,5 @@ const hamburgerButton = document.getElementById('hamburger-button');
 const navBarLinks = document.getElementById('navbar-links');
 
 hamburgerButton.addEventListener('click', () => {
-    console.log('butt')
     navBarLinks.classList.toggle('active')
 })
